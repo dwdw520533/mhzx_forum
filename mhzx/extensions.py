fabd2b5b00b65364b8ab0ec1b34bad43ebaa87bf
dongwei <>
2019-11-25 16:12:01 +0800

    bugfix

--- mhzx/extensions.py
+++ mhzx/extensions.py
@@ -83,7 +83,7 @@ def init_extensions(app):
     #configure_uploads(app, upload_photos)
     #mail.init_app(app)
     admin.init_app(app)
-    mongo.init_app(app, "MONGO_URI")
+    mongo.init_app(app, "MONGO")
     #oauth.init_app(app)
     #login_manager.init_app(app)
     # if app.config.get('USE_CACHE', False):
