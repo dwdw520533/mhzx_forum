e0a5b4f30f7879de2527e9a21f9cb641553981b9
dongwei <>
2019-11-26 13:58:55 +0800

    bugfix

--- mhzx/extensions.py
+++ mhzx/extensions.py
@@ -17,24 +17,24 @@ from whoosh.fields import Schema, TEXT, ID, DATETIME
 
 logger = logging.getLogger(__name__)
 # 初始化Mail
-mail = Mail()
+#mail = Mail()
 # 初始化Flask-Admin
-admin = Admin(name='后台管理')
+#admin = Admin(name='后台管理')
 mongo = PyMongo()
-login_manager = LoginManager()
-login_manager.login_view = 'user.login'
+#login_manager = LoginManager()
+#login_manager.login_view = 'user.login'
 
 # 图片上传
-upload_photos = UploadSet('photos')
+#upload_photos = UploadSet('photos')
 
 # Cache
 # cache = Cache()
 
 # OAuth
-oauth = OAuth()
+#oauth = OAuth()
 #oauth_weibo = oauth.remote_app('weibo', )
 
-whoosh_searcher = WhooshSearcher()
+#whoosh_searcher = WhooshSearcher()
 
 
 # def get_user_credit_balance(user):
@@ -82,25 +82,25 @@ def init_extensions(app):
     #whoosh_searcher.init_app(app)
     #configure_uploads(app, upload_photos)
     #mail.init_app(app)
-    admin.init_app(app)
+    #admin.init_app(app)
     mongo.init_app(app, "MONGO")
     #oauth.init_app(app)
     #login_manager.init_app(app)
     # if app.config.get('USE_CACHE', False):
     #     cache.init_app(app, {})
 
-    with app.app_context():
+    #with app.app_context():
         # 添加flask-admin视图
-        admin.add_view(admin_view.UsersModelView(mongo.db['users'], '用户管理'))
-        admin.add_view(admin_view.CatalogsModelView(mongo.db['catalogs'], '栏目管理'))
-        admin.add_view(admin_view.PostsModelView(mongo.db['posts'], '帖子管理'))
-        admin.add_view(admin_view.PassagewaysModelView(mongo.db['passageways'], '温馨通道'))
-        admin.add_view(admin_view.FriendLinksModelView(mongo.db['friend_links'], '友链管理'))
-        admin.add_view(admin_view.PagesModelView(mongo.db['pages'], '页面管理'))
-        admin.add_view(admin_view.FooterLinksModelView(mongo.db['footer_links'], '底部链接'))
-        admin.add_view(admin_view.AdsModelView(mongo.db['ads'], '广告管理'))
-        admin.add_view(admin_view.OptionsModelView(mongo.db['options'], '系统设置'))
-        admin.add_view(admin_view.ProductModelView(Product, name='商品管理'))
+        # admin.add_view(admin_view.UsersModelView(mongo.db['users'], '用户管理'))
+        # admin.add_view(admin_view.CatalogsModelView(mongo.db['catalogs'], '栏目管理'))
+        # admin.add_view(admin_view.PostsModelView(mongo.db['posts'], '帖子管理'))
+        # admin.add_view(admin_view.PassagewaysModelView(mongo.db['passageways'], '温馨通道'))
+        # admin.add_view(admin_view.FriendLinksModelView(mongo.db['friend_links'], '友链管理'))
+        # admin.add_view(admin_view.PagesModelView(mongo.db['pages'], '页面管理'))
+        # admin.add_view(admin_view.FooterLinksModelView(mongo.db['footer_links'], '底部链接'))
+        # admin.add_view(admin_view.AdsModelView(mongo.db['ads'], '广告管理'))
+        # admin.add_view(admin_view.OptionsModelView(mongo.db['options'], '系统设置'))
+        # admin.add_view(admin_view.ProductModelView(Product, name='商品管理'))
 
         # 初始化Whoosh索引
         # chinese_analyzer = ChineseAnalyzer()
